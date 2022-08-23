fd34cc8654c5067fbd1416d3179fa4e2ef256c1f
cordial999 <1932844332@qq.com>
2022-08-23 09:10:17 +0800

    babyGrow1

---
+++ babyGrow/uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\software\微信web开发者工具\cli.bat" cloud functions deploy --e dada-8gjy4y9xf021dd0e --n quickstartFunctions --r --project "E:\mini\babyGrow" --report_first && "D:\software\微信web开发者工具\cli.bat" cloud functions deploy --e test-5g2yd1m1a04e90ad --n quickstartFunctions --r --project "E:\mini\babyGrow" --report
